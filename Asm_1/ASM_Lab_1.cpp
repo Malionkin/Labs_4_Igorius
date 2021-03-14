1311c98d82d7ac1b928fae6a3831a2509214d717
Yakov <malionkinjr@gmail.com>
2021-03-15 00:26:22 +0300

    Asm_1
    Finally

---
+++ Asm_1/ASM_Lab_1.cpp
@@ -0,0 +1,109 @@
+﻿/*#include <iostream>
+using namespace std;
+
+int Calculate(int a, int b, int c, int y)
+{
+	int result;
+
+	__asm
+	{
+		mov eax, a 
+		imul eax, 3
+		mov ecx, eax
+		mov eax, b
+		mov ebx, y
+		imul eax, ebx
+		mov ebx, c
+		add eax, ebx
+		neg eax
+		cdq
+		idiv ecx
+		mov result, eax
+		
+	}
+	cout << "Solution is " << result << endl;
+	return result;
+}
+
+
+
+
+
+int main()
+{
+	cout << "Please, enter coeffs a, b, y, c!";
+	int a, b, y, c;
+	cin >> a; cin >> b; cin >> y;cin >>  c;
+	Calculate(a, b, c, y);
+	return 0;
+}*/
+#include <iostream>
+
+using namespace std;
+
+int Calculate(int a, int b, int c, int y)
+{
+    int result;
+    __asm
+    {
+        mov eax, a
+        imul eax, 3
+        mov ecx, eax
+
+        mov eax, b
+        mov ebx, y
+        imul eax, ebx
+
+        mov ebx, c
+        add eax, ebx
+        neg eax
+        cdq
+        idiv ecx
+
+        cmp edx, 0
+        jne rnd
+        jmp end_if
+
+        rnd :
+        imul edx, 2
+            cmp edx, ecx
+            jg increase
+            jmp skip
+
+            increase :
+        inc eax
+            jmp end_if
+            skip :
+
+        cmp edx, ecx
+            jl end_if
+
+            mov ecx, eax
+            mov ebx, 2
+            cdq
+            idiv ebx
+            cmp edx, 0
+            je end_if
+            mov eax, ecx
+            inc eax
+
+            end_if :
+        mov result, eax
+    }
+    return result;
+}
+
+int main() {
+    int a, b, c, y;
+    cout << "Enter a, b, c, y: ";
+    cin >> a; cin >> b; cin >> c; cin >> y;
+    cout << Calculate(a, b, c, y) << endl;
+    return 0;
+}
+
+
+
+
+
+
+
