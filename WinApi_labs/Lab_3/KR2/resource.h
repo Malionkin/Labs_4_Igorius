b06ae7f3eb1a68844567960ab517c4a8472bb194
Malionkin <ymalyonkin@bk.ru>
2021-05-21 01:01:35 +0300

    HZ

---
+++ WinApi_labs/Lab_3/KR2/resource.h
@@ -0,0 +1,42 @@
+//{{NO_DEPENDENCIES}}
+// Включаемый файл, созданный в Microsoft Visual C++.
+// Используется KR2.rc
+//
+#define IDC_MYICON                      2
+#define IDC_LIST                        2
+#define IDD_KR2_DIALOG                  102
+#define IDS_APP_TITLE                   103
+#define IDD_ABOUTBOX                    103
+#define IDM_ABOUT                       104
+#define IDM_EXIT                        105
+#define IDI_KR2                         107
+#define IDI_SMALL                       108
+#define IDC_KR2                         109
+#define IDR_MAINFRAME                   128
+#define IDD_DIALOG1                     129
+#define IDC_SIZE_EDIT                   1000
+#define IDC_PROGRESS1                   1001
+#define IDC_SLIDER1                     1002
+#define IDC_SPIN1                       1003
+#define IDC_SIZE_SLIDER                 1004
+#define ID_SIZE_OK                      1005
+#define IDC_LIST4                       1010
+#define IDC_BUTTON_PLUS1                1013
+#define IDC_BUTTON_MINUS                1014
+#define IDC_BUTTON_CLEAR                1015
+#define IDC_TEXT                        1016
+#define ID_32771                        32771
+#define ID_CHANGE_SIZE_MENU             32772
+#define IDC_STATIC                      -1
+
+// Next default values for new objects
+// 
+#ifdef APSTUDIO_INVOKED
+#ifndef APSTUDIO_READONLY_SYMBOLS
+#define _APS_NO_MFC                     1
+#define _APS_NEXT_RESOURCE_VALUE        130
+#define _APS_NEXT_COMMAND_VALUE         32773
+#define _APS_NEXT_CONTROL_VALUE         1017
+#define _APS_NEXT_SYMED_VALUE           110
+#endif
+#endif
